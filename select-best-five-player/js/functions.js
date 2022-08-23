f963344e052dc534a37179e46f14992f90b0e1a9
webdev-chinmoy-biswas <chinmoy709@gmail.com>
2022-08-23 19:46:30 +0600

    added functionns

--- select-best-five-player/js/functions.js
+++ select-best-five-player/js/functions.js
@@ -4,3 +4,31 @@ function addPlayerName(event) {
   list.innerHTML += `<li>${player}</li>`;
   event.target.disabled = true;
 }
+
+function calculateTotalPlayerCost() {
+  const totalSelectedPlayer = document.querySelectorAll('#selected-player-list li').length;
+  const playerValue = parseFloat(perPlayerInputField.value);
+
+  if(playerValue > 0 ) {
+   playerExpense.innerText = totalSelectedPlayer * playerValue;
+  }
+  else {
+    alert('Please Enter a number')
+  }
+
+}
+
+function calculateTotal() {
+  const managerCost = parseFloat(document.getElementById('manager').value);
+  const coachCost = parseFloat(document.getElementById('coach').value);
+  const playerCost = parseFloat(playerExpense.innerText)
+
+  if(managerCost > 0 && coachCost > 0 ) {
+    totalCost.innerText = playerCost + managerCost + coachCost;
+   }
+   else {
+     alert('Please Enter Cost in number');
+   }
+}
+
+
--- select-best-five-player/js/script.js
+++ select-best-five-player/js/script.js
@@ -1,8 +1,13 @@
 const selectButtons = document.getElementsByClassName('select-btn');
+const perPlayerInputField = document.getElementById('per-player-cost');
+const calculateBtn = document.getElementById('calculate-btn');
+const calculateTotalBtn = document.getElementById('calculate-total-btn');
+const playerExpense = document.getElementById('player-expense');
+const totalCost = document.getElementById('total-cost');
 
 for(const button of selectButtons) {
     button.addEventListener('click', function(event){
-        const list = document.querySelectorAll('#selected-player-list li')
+        const list = document.querySelectorAll('#selected-player-list li');
         if( list.length < 5) {
             addPlayerName(event);
         } else {
@@ -10,3 +15,13 @@ for(const button of selectButtons) {
         }
     });
 }
+
+calculateBtn.addEventListener('click', function(){
+    calculateTotalPlayerCost();
+    // perPlayerInputField.value = '';
+})
+
+calculateTotalBtn.addEventListener('click', function(){
+    calculateTotal();
+})
+
